86478f4d00f953eb90c70c24724149fdc6ec4fd7
w40141 <w.40141@gmail.com>
2022-11-01 19:17:13 +0900

    edit

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,5 @@
+import {type} from "os";
+
 function sayHello(firstName: string) {
     console.log('Hello ' + firstName);
 }
@@ -27,3 +29,50 @@ const printName = (obj: {firstName: string; lastName?: string}) => {
 
 printName({firstName: "Takuya", lastName: 'Tejima'})
 printName({firstName: "Takuya"})
+
+function sayHello2(name: string): string {
+    return `Hello ${name}`
+}
+
+function sayHello3(name: string, greeting?: string): string {
+    return `${greeting} ${name}`
+}
+
+const sayHello4 = (name: string): string => `Hello ${name}`
+
+const genBirdsInfo = (name: string): string[] => name.split(',')
+const singBirds = (birdInfo: (x: string) => string[]): string => birdInfo('hato, kiji')[0] + 'piyp piyo'
+
+type Name = string
+type Point = {
+    x: number;
+    y: number;
+}
+
+function distance(point: Point) {
+    const x = point.x
+    const y = point.y
+    return x * x + y * y
+}
+
+type Label = {
+    [key: string]: string
+}
+
+interface PPoint {
+    x: number
+    y: number
+    printt: () => string
+}
+
+class MyPoint implements PPoint {
+    x: number;
+    y: number;
+    constructor(_x: number = 0, _y: number = 0) {
+        this.x = _x
+        this.y = _y
+    }
+    printt(): string {
+        return `${this.x}, ${this.y}`
+    }
+}
